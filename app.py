02f8b76bdc186da64c6217daf9e507beaaa7cb6e
Leandrito <castromoneda7@gmail.com>
2024-11-02 16:50:19 -0400

    Añadiendo login y encriptacion de la contraseña al registrarse

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask,request,jsonify
 import sqlite3
+import jwt
+import bcrypt
+from datetime import datetime,timedelta
 
 app = Flask(__name__)
+key = "shave"
+
+def deco(f):
+    def wrapper(*args,**kwargs):
+        try:
+            token=request.headers["Authorization"].split(" ")[1]
+            jwt.decode(token,key,algorithms=["HS256"])
+        except Exception as e:
+            print(str(e))
+            return jsonify({"error":"Issue with token"})
+        return f()
+    return wrapper
 
 def openDb():
     with sqlite3.connect("todo eipiai/tododb") as conn:
@@ -19,6 +34,7 @@ def register():
     if userexists:
         return jsonify("User or email already in use")
     else:
+        password = bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt())
         conn = openDb()
         cursor = conn.cursor()
         cursor.execute("""INSERT INTO users (name,email,password) VALUES (?,?,?)""", (name,email,password,))
@@ -28,7 +44,21 @@ def register():
     
 @app.route("/login", methods=["POST"])
 def login():
-    pass
+    email,password = request.json["email"],request.json["password"]
+    
+    conn = openDb()
+    cursor = conn.cursor()
+    
+    userexists = cursor.execute("SELECT name,email,password FROM users WHERE email = (?)", (email,)).fetchone()
+    
+    if userexists and bcrypt.checkpw(password.encode('utf-8'),userexists[2]):
+        #token
+        expiration_time = datetime.utcnow() + timedelta(minutes=20)
+        token = jwt.encode({"name":userexists[0],"exp":expiration_time},key)
+        return jsonify({"token":f"Bearer {token}"})
+    else:
+        return jsonify({"error":"Authentication failed"})
+    
 
 
 if __name__ == "__main__":
